bc4bd46296459652d79ddccdfcbbea1e8dc18231
mv.baranov <mv.baranov@tensor.ru>
2022-12-03 19:35:03 +0300

    Add water object

---
+++ src/lib/components/Objects/water.ts
@@ -0,0 +1,48 @@
+import { Vector2D } from "../Vector";
+import type { IRectangleSize } from "./Interfaces";
+import { GameObject, type IRectangular } from "./GameObject";
+
+export default class Water extends GameObject implements IRectangular {
+	// Implements IRectangular interface
+	width: number;
+	height: number;
+
+	textures: HTMLImageElement;
+
+	constructor(vCoordinates: Vector2D, size: IRectangleSize, textures: HTMLImageElement) {
+		super(vCoordinates, new Vector2D());
+
+		this.width = size.width;
+		this.height = size.height;
+		this.textures = textures;
+	}
+
+	draw = (context: CanvasRenderingContext2D, viewPortHeight: number, vViewCoordinates: Vector2D): void => {
+		const viewCoords = this.vCoordinates.getViewCoordinates(viewPortHeight);
+		let height = this.height;
+		let row = 0;
+		while (height > 0) {
+			let width = this.width;
+			let column = 0;
+
+			while (width > 0) {
+				context.drawImage(
+					this.textures,
+					0, 0,
+					Math.min(128, width),
+					Math.min(128, height),
+					128 * column + viewCoords.x - vViewCoordinates.x,
+					128 * row + viewCoords.y - this.height + vViewCoordinates.y,
+					Math.min(128, width),
+					Math.min(128, height)
+				);
+
+				width -= 128;
+				column++;
+			}
+
+			height -= 128;
+			row++;
+		}
+	}
+}
